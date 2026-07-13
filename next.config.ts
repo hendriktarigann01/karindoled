4a270907ad2ad9fd27038114bb6600f5055145a6
hendriktarigann01 <hendriktarigan25@gmail.com>
2026-07-13 08:59:17 +0700

    build: comment out basePath for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio/karindo",
+  // basePath: "/portfolio/karindo",
   images: {
     unoptimized: true,
     formats: ["image/webp", "image/avif"],
